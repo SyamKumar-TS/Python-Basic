180bfdc7b05f0574c19d9a0d5735f3c3914389dd
Syam <syamk2222@gmail.com>
2025-05-27 21:48:11 +0530

    updated

--- c/test.py
+++ c/test.py
@@ -1,10 +1,12 @@
-def prime():
-   n=int(input("enter num:"))
-   fact=1
-   for i in range(1,n+1):
-      fact=fact*i
-   print(fact)      
-          
-prime()      
-            
+li=list(map(int,input("enter list:").split( )))
+l=len(li)
+target_sum=7
+count=0
+for i in range(l-1):
+   for j in range (l-1):
       
+     sum=li[i]+li[j+1]
+     if target_sum==sum:
+        
+        count=count+1
+print(count)   
